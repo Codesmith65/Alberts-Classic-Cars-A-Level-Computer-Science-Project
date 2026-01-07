30805de2ff649883d48f1251876093034e8806fc
Codesmith65 <187884700+Codesmith65@users.noreply.github.com>
2026-01-07 19:43:18 +0000

    First implmentation of booking data structure

---
+++ dataTypes/booking.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from uuid import UUID, uuid4
+
+
+@dataclass
+class Booking:
+	staffID: UUID
+	clientID: UUID
+	vehicleID: UUID
+	pickupDate: int
+	pickupLocation: UUID
+	dropoffDate: int
+	dropoffLocation: UUID
+	bookingID: UUID = uuid4()
+
+	def getAtributes(self) -> list[UUID|int]:
+		return [self.bookingID, self.staffID, self.clientID, self.vehicleID, self.pickupDate, self.pickupLocation, self.dropoffDate, self.pickupLocation]

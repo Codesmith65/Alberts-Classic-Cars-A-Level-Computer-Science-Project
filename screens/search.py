e435fc2410da9a4f1d54f6200ca96cab5fbe7ace
22140733 <22140733@DS-L-107-46711>
2026-01-09 10:33:25 +0000

    First implementation of ui

---
+++ screens/search.py
@@ -0,0 +1,45 @@
+from sys import setdlopenflags
+import tkinter as tk
+from tkinter import ttk
+from .generic import Generic
+
+
+class Search(Generic):
+	def __init__(self) -> None:
+		super().__init__()
+
+		self.root.title("Albert's Classic Car - Search")
+
+		self.topBarFrame = tk.Frame(self.root)
+		self.mainContectFrame = tk.Frame(self.root)
+
+		self.companyLogo = tk.PhotoImage
+		self.companyLogoLabel = tk.Label(self.topBarFrame)
+		self.titleLabel = tk.Label(self.topBarFrame)
+		self.logOutButton = tk.Button(self.topBarFrame)
+		
+		self.searchEntry = tk.Entry(self.mainContectFrame)
+		self.SearchButton = tk.Entry(self.mainContectFrame)
+		
+		self.searchResultsLabelFrame = tk.LabelFrame(self.mainContectFrame)
+		
+
+		self.companyLogoLabel["text"] = "logo"
+		self.titleLabel["text"] ="Search"
+		self.titleLabel["font"] = ("Helvetica", 40)
+		self.logOutButton["text"] = "Logout"
+		
+		self.SearchButton["text"] = "s"
+		self.searchResultsLabelFrame["text"] = "Search Results"
+		
+
+		self.topBarFrame.pack()
+		self.mainContectFrame.pack()
+
+		self.companyLogoLabel.pack()
+		self.titleLabel.pack()
+		self.logOutButton.pack()
+		
+		self.searchEntry.pack()
+		self.SearchButton.pack()
+		self.searchResultsLabelFrame.pack()

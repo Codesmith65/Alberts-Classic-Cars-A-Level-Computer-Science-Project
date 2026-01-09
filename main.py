cb768a86bdab86b9f885e5892484d4bff941440a
22140733 <22140733@DS-L-107-46711>
2026-01-09 14:04:01 +0000

    Tempeary code to create or check user file

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from screens.account import Account
 testWindow = Account()
 
 testWindow.mainLoop()
+
+from dataTypes.user import User
+import pickle
+
+
+with open("data/users.pkl", "rb") as f:
+    users = pickle.load(f)
+    for user in users:
+        print(user.getAtributes())

cf023c34f57eaee92f4aad190c4c2d187a67572c
22140733 <22140733@DS-L-107-46711>
2026-01-09 14:42:28 +0000

    added loging in ability

--- application.py
+++ application.py
@@ -4,3 +4,6 @@ from uuid import UUID
 class Application:
     def __init__(self) -> None:
         self.logedInUser: UUID|None = None
+    
+    def setLoggedInUser(self, userID: UUID|None) -> None:
+        self.logedInUser = userID

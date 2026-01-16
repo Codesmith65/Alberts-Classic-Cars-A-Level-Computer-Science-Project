2130af66b798914325597d17da742b4aed25b936
22140733 <22140733@DS-L-107-46711>
2026-01-16 15:22:17 +0000

    Added return home feature

--- screens/search.py
+++ screens/search.py
@@ -1,5 +1,8 @@
+from re import S
 import tkinter as tk
 from tkinter import ttk
+
+import screens
 from .generic import GenericScreen
 from application import Application
 
@@ -14,7 +17,7 @@ class Search(GenericScreen):
 		self.mainContectFrame = tk.Frame(self.root)
 
 		self.companyLogo = tk.PhotoImage
-		self.companyLogoLabel = tk.Label(self.topBarFrame)
+		self.companyLogoLabel = tk.Button(self.topBarFrame)
 		self.titleLabel = tk.Label(self.topBarFrame)
 		self.logOutButton = tk.Button(self.topBarFrame)
 		
@@ -32,6 +35,8 @@ class Search(GenericScreen):
 		self.SearchButton["text"] = "s"
 		self.searchResultsLabelFrame["text"] = "Search Results"
 		
+		self.companyLogoLabel["command"] = self.goHome
+		
 
 		self.topBarFrame.pack()
 		self.mainContectFrame.pack()
@@ -43,3 +48,6 @@ class Search(GenericScreen):
 		self.searchEntry.pack()
 		self.SearchButton.pack()
 		self.searchResultsLabelFrame.pack()
+	
+	def goHome(self) -> None:
+		self.application.switchForm(screens.Home)

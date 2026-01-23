134f23d56debe42b1a3976613dedb671d9a0fb39
22140733 <22140733@DS-L-107-46711>
2026-01-23 15:45:51 +0000

    Tried to add scroll bar, dosent work :(

--- screens/search.py
+++ screens/search.py
@@ -1,6 +1,7 @@
+import pickle
 from re import S
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 
 import screens
 from .generic import GenericScreen
@@ -10,6 +11,8 @@ from application import Application
 class Search(GenericScreen):
 	def __init__(self, application: Application) -> None:
 		super().__init__(application)
+		
+		self.mode = 0
 
 		self.root.title("Albert's Classic Car - Search")
 
@@ -17,15 +20,27 @@ class Search(GenericScreen):
 		self.mainContectFrame = tk.Frame(self.root)
 
 		self.companyLogo = tk.PhotoImage(file="assets/logo.png")
+		self.searchIcon = tk.PhotoImage(file="assets/serach.png")
+		self.infoIcon = tk.PhotoImage(file="assets/info.png")
 		self.companyLogoHomeButton = tk.Button(self.topBarFrame)
 		self.titleLabel = tk.Label(self.topBarFrame)
 		self.logOutButton = tk.Button(self.topBarFrame)
 		
 		self.searchEntry = tk.Entry(self.mainContectFrame)
 		self.SearchButton = tk.Button(self.mainContectFrame)
+		self.canvas: tk.Canvas = tk.Canvas(self.mainContectFrame)
 		
-		self.searchResultsLabelFrame = tk.LabelFrame(self.mainContectFrame)
+		self.searchResultsLabelFrame = tk.LabelFrame(self.canvas)
+		self.scrollBar: tk.Scrollbar = tk.Scrollbar(self.mainContectFrame)
+
 		
+
+		self.canvas.config(yscrollcommand=self.scrollBar.set)
+		self.canvas.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+		
+		self.searchResultsLabelFrame["width"] = 100
+		self.searchResultsLabelFrame["height"] = 100
+
 		self.companyLogoHomeButton["image"] = self.companyLogo
 		self.companyLogoHomeButton["relief"] = "flat"
 		self.companyLogoHomeButton["borderwidth"] = 0
@@ -36,15 +51,16 @@ class Search(GenericScreen):
 		self.titleLabel["font"] = ("Helvetica", 40)
 		self.logOutButton["text"] = "Logout"
 		
-		self.SearchButton["text"] = "s"
+		self.SearchButton["image"] = self.searchIcon
 		self.searchResultsLabelFrame["text"] = "Search Results"
 		
 		self.companyLogoHomeButton["command"] = self.goHome
 		self.SearchButton["command"] = self.search
 		
+		self.canvas.create_window((0, 0), window=self.searchResultsLabelFrame, anchor="nw")
 
 		self.topBarFrame.pack()
-		self.mainContectFrame.pack()
+		self.mainContectFrame.pack(fill=tk.BOTH, expand=1)
 
 		self.companyLogoHomeButton.pack()
 		self.titleLabel.pack()
@@ -52,11 +68,13 @@ class Search(GenericScreen):
 		
 		self.searchEntry.pack()
 		self.SearchButton.pack()
-		self.searchResultsLabelFrame.pack()
+		self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+		self.scrollBar.pack(side=tk.RIGHT, fill=tk.Y)
+		
 	
 	def goHome(self) -> None:
 		self.application.switchForm(screens.Home)
-
+	
 
 	def createSearchResult(self, title: str, subTitle: str, data: dict) -> None:
 		infoText: str = f"{title} Data\n"

d07efd73da0d8b0e242df42d3b47cca7ea8896ea
22140733 <22140733@DS-L-107-46711>
2026-01-23 15:39:48 +0000

    Added search function and added search results to list

--- screens/search.py
+++ screens/search.py
@@ -40,6 +40,7 @@ class Search(GenericScreen):
 		self.searchResultsLabelFrame["text"] = "Search Results"
 		
 		self.companyLogoHomeButton["command"] = self.goHome
+		self.SearchButton["command"] = self.search
 		
 
 		self.topBarFrame.pack()
@@ -55,3 +56,55 @@ class Search(GenericScreen):
 	
 	def goHome(self) -> None:
 		self.application.switchForm(screens.Home)
+
+
+	def createSearchResult(self, title: str, subTitle: str, data: dict) -> None:
+		infoText: str = f"{title} Data\n"
+		
+		for key in data:
+			infoText += f"\n{key}: {data[key]}"
+
+		searchResultFrame = tk.Frame(self.searchResultsLabelFrame)
+		titleLable: tk.Label = tk.Label(searchResultFrame, text=title)
+		subTitle: tk.Label = tk.Label(searchResultFrame, text=subTitle)
+		infoButton: tk.Button = tk.Button(searchResultFrame, image=self.infoIcon, command=lambda infoText=infoText: messagebox.showinfo(title, infoText))
+		
+		titleLable.grid(row=0,column=0)
+		subTitle.grid(row=1, column=0)
+		infoButton.grid(row=1,column=1)
+		searchResultFrame.pack()
+
+
+	def search(self) -> None:
+		searchCriteria: str = self.searchEntry.get()
+		
+		if searchCriteria == "":
+			messagebox.showwarning("Invalid search criteria", "Please enter a seach criteria to serach with")
+		
+		if self.mode == 0 or self.mode == 1:
+			foundUsers: list[Tuple[str, list]] = self.__linearSearchFile("data/users.pkl", searchCriteria)
+			for user in foundUsers:
+				self.createSearchResult("User", user[0], dict(zip(["ID", "Username"], user[1])))
+			
+	
+	def __linearSearchFile(self, path: str, criteria: str) -> list[tuple[str, list]]:
+		with open(path, "rb") as file:
+			entries: list = pickle.load(file)
+				
+
+		print(entries)
+			
+		if not(type(entries) is list):
+			messagebox.showerror("User Data", "There is an issue with the user data stored")
+		
+		results: list[Tuple[str, list]] = []
+
+		for entry in entries:
+			entryAttributes: list[str] = entry.getAtributes()
+			
+			for attribute in entryAttributes:
+				if criteria in str(attribute):
+					results.append((str(attribute), entryAttributes))
+					continue
+		
+		return results

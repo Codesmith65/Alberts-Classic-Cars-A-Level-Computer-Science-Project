f17f01f254667f64bbda9cbf55d8d3e9d6c9bb11
22140733 <22140733@DS-L-107-46711>
2026-02-06 15:08:25 +0000

    Added pick up and drop off inputs

--- screens/booking.py
+++ screens/booking.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sqlite3 import enable_callback_tracebacks
 import tkinter as tk
 from tkinter import StringVar, ttk
@@ -32,18 +33,21 @@ class Booking(GenericScreen):
 		self.vehicleEntry = tk.Entry(self.mainContectFrame)
 		self.vehicleSearch = tk.Button(self.mainContectFrame)
 		
-		self.clientEntryStringVar = tk.StringVar()
-		self.vehicleEntryStringVar = tk.StringVar()
+		self.clientEntryStringVar: tk.StringVar = tk.StringVar()
+		self.vehicleEntryStringVar: tk.StringVar = tk.StringVar()
 		
 		self.pickUpDateLabelFrame = tk.LabelFrame(self.mainContectFrame)
 		self.dropOffDateLabelFrame = tk.LabelFrame(self.mainContectFrame)
 		
-		self.bookButton = tk.Button(self.mainContectFrame)
+		self.pickUpDay: ttk.Combobox = ttk.Combobox(self.pickUpDateLabelFrame)
+		self.pickUpMonth: ttk.Combobox = ttk.Combobox(self.pickUpDateLabelFrame)
+		self.pickUpYear: ttk.Combobox = ttk.Combobox(self.pickUpDateLabelFrame)
 		
-		self.testButton1 = tk.Button(self.pickUpDateLabelFrame, text="test")
-		self.testButton1.pack()
-		self.testButton1 = tk.Button(self.dropOffDateLabelFrame, text="test")
-		self.testButton1.pack()
+		self.dropOffDay: ttk.Combobox = ttk.Combobox(self.dropOffDateLabelFrame)
+		self.dropOffMonth: ttk.Combobox = ttk.Combobox(self.dropOffDateLabelFrame)
+		self.dropOffYear: ttk.Combobox = ttk.Combobox(self.dropOffDateLabelFrame)
+		
+		self.bookButton = tk.Button(self.mainContectFrame)
 
 
 		self.companyLogoHomeButton["image"] = self.companyLogo
@@ -71,6 +75,29 @@ class Booking(GenericScreen):
 		self.dropOffDateLabelFrame["text"] = "Drop off Date"
 		
 		self.companyLogoHomeButton["command"] = self.goHome
+		
+		self.pickUpDay["state"] = "readonly"
+		self.pickUpMonth["state"] = "readonly"
+		self.pickUpYear["state"] = "readonly"
+		
+		self.dropOffDay["state"] = "readonly"
+		self.dropOffMonth["state"] = "readonly"
+		self.dropOffYear["state"] = "readonly"
+		
+		days = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20","21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
+		months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+		currentYear = current_year = datetime.now().year
+		years = []
+		for x in range(5):
+			years.append(str(currentYear + x))
+	
+		self.pickUpDay["values"] = days
+		self.pickUpMonth["values"] = months
+		self.pickUpYear["values"] = years
+		
+		self.dropOffDay["values"] = days
+		self.dropOffMonth["values"] = months
+		self.dropOffYear["values"] = years
 
 
 		self.topBarFrame.pack()
@@ -87,11 +114,22 @@ class Booking(GenericScreen):
 		self.vehicleLabel.pack()
 		self.vehicleEntry.pack()
 		self.vehicleSearch.pack()
+
+		self.pickUpDay.pack()
+		self.pickUpMonth.pack()
+		self.pickUpYear.pack()
+		
+		self.dropOffDay.pack()
+		self.dropOffMonth.pack()
+		self.dropOffYear.pack()
 		
 		self.pickUpDateLabelFrame.pack()
 		self.dropOffDateLabelFrame.pack()
 
+		self.bookButton.pack()
+	
 		self.mainContectFrame.pack()
+		
 	
 	def goHome(self) -> None:
 		self.application.switchForm(screens.Home)

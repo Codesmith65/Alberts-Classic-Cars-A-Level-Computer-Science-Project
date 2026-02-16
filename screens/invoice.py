50af4dba7b387590a9fe9a19079c7a33dfaee14d
Codesmith65 <187884700+Codesmith65@users.noreply.github.com>
2026-02-16 16:48:02 +0000

    Added functionality to screen

--- screens/invoice.py
+++ screens/invoice.py
@@ -1,8 +1,13 @@
 from re import S
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from .generic import GenericScreen
 from application import Application
+from uuid import UUID
+from datetime import datetime
+from dataTypes.booking import Booking
+import pickle
+import screens
 
 
 class Invoice(GenericScreen):
@@ -17,7 +22,7 @@ class Invoice(GenericScreen):
 		self.companyLogo = tk.PhotoImage
 		self.companyLogoLabel = tk.Label(self.topBarFrame)
 		self.titleLabel = tk.Label(self.topBarFrame)
-		self.logOutButton = tk.Button(self.topBarFrame)
+		self.homeButton = tk.Button(self.topBarFrame)
 		
 		self.BookingIDLabel = tk.Label(self.mainContectFrame)
 		self.pickUpLabel = tk.Label(self.mainContectFrame)
@@ -28,22 +33,51 @@ class Invoice(GenericScreen):
 		self.balanceLabel = tk.Label(self.mainContectFrame)
 		self.balanceDueLabel = tk.Label(self.mainContectFrame)
 		self.statusLabel = tk.Label(self.mainContectFrame)
+
+
+		if not "BookingID" in self.application.crossScreenDataStore:
+			self.BookingIDLabel["text"] = "BookingID: No booking inputed"
+			messagebox.showerror("Booking ID", "No booking ID has been passed to this form, try going back and reloading form, if persists contact administartor")
 		
+		self.bookingID: UUID = self.application.crossScreenDataStore["BookingID"]
+		self.booking: Booking|None = None
+
+		with open("data/bookings.pkl", "br") as bookingsFile:
+			bookings: list[Booking] = pickle.load(bookingsFile)
+
+			if type(bookings) is not list:
+				messagebox.showerror("Booking data storage", "There is an issue with the storage of booking data, contact administartor")
+
+			for booking in bookings:
+				if booking.bookingID == self.bookingID:
+					self.booking = booking
+					break
+			
+			if self.booking is None:
+				messagebox.showerror("Booking not found", "The requested booking was not found, please contact the administrator")
+				return #Placeholder, this functionality should be abstracted to function so ui can be created regardless if booking is found or not
+		
+		bookingDuration = (self.booking.dropoffDate - self.booking.pickupDate) / 86400
+		bookingCost = bookingDuration * 20 #Cost of booking a car per day
+		bookingDeposit = bookingCost * 0.1 #Deposite is 10% of booking cost
+		bookingBalence = bookingCost - bookingDeposit
+
 
 		self.companyLogoLabel["text"] = "logo"
 		self.titleLabel["text"] ="Invoice"
 		self.titleLabel["font"] = ("Helvetica", 40)
-		self.logOutButton["text"] = "Logout"
+		self.homeButton["text"] = "Home"
+		self.homeButton["command"] = self.__goHome
 
-		self.BookingIDLabel["text"] = "BookingID:"
-		self.pickUpLabel["text"] = "Pickup:"
-		self.dropOffLabel["text"] = "Drop off:"
-		self.costLabel["text"] = "Cost:"
-		self.depositeLabel["text"] = "Deposite:"
+		self.BookingIDLabel["text"] = f"BookingID: {self.bookingID}"
+		self.pickUpLabel["text"] = f"Pickup: {datetime.fromtimestamp(self.booking.pickupDate):%d/%m/%Y}"
+		self.dropOffLabel["text"] = f"Drop off: {datetime.fromtimestamp(self.booking.dropoffDate):%d/%m/%Y}"
+		self.costLabel["text"] = f"Cost: £{bookingCost:.2f}"
+		self.depositeLabel["text"] = f"Deposite: £{bookingDeposit:.2f}"
 		self.depositeDueLabel["text"] = "Due:"
-		self.balanceLabel["text"] = "Balance:"
+		self.balanceLabel["text"] = f"Balance: £{bookingBalence:.2f}"
 		self.balanceDueLabel["text"] = "Due:"
-		self.statusLabel["text"] = "Status:"
+		self.statusLabel["text"] = f"Status: {self.booking.status}"
 
 
 		self.topBarFrame.pack()
@@ -51,7 +85,7 @@ class Invoice(GenericScreen):
 
 		self.companyLogoLabel.pack()
 		self.titleLabel.pack()
-		self.logOutButton.pack()
+		self.homeButton.pack()
 		
 		self.BookingIDLabel.pack()
 		self.pickUpLabel.pack()
@@ -62,3 +96,7 @@ class Invoice(GenericScreen):
 		self.balanceLabel.pack()
 		self.balanceDueLabel.pack()
 		self.statusLabel.pack()
+	
+
+	def __goHome(self):
+		self.application.switchForm(screens.Home)

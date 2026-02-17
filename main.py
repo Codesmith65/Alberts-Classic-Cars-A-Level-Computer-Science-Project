1609ec3477247a65965bca6d14a0d075c0526e5c
Codesmith65 <187884700+Codesmith65@users.noreply.github.com>
2026-02-17 16:20:22 +0000

    Set start screen to login and cleaned up

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
 import screens
 from application import Application
-import tkinter as tk
-
 
 
 application = Application()
-application.switchForm(screens.Booking)
-
+application.switchForm(screens.Login)
 
 while application.running:
     application.currentScreen.mainLoop()
-
-
-#testWindow = Login(application)
-
-#testWindow.mainLoop()
-
-from dataTypes.user import User
-import pickle
-
-users=[]
-with open("data/users.pkl", "rb") as f:
-    users = pickle.load(f)
-    for user in users:
-        print(user.getAtributes())

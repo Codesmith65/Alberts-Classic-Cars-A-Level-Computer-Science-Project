bc6b18f1bde4b7f1a8219587902cca4eb50df4f2
22140733 <22140733@DS-L-107-46711>
2026-03-20 13:38:48 +0000

    Added create new client

--- popups/clientEdit.py
+++ popups/clientEdit.py
@@ -10,7 +10,8 @@ class ClientEdit:
     # Initilaistion function to creat the screen
     def __init__(self, data) -> None:
         # Open the data file find the data type
-        with open("data/clients.pkl", "rb") as dataFile:
+        self.filePath = "data/clients.pkl"
+        with open(self.filePath, "rb") as dataFile:
             dataTypes = pickle.load(dataFile)
         
         # Find the current user to edit in the file and its index
@@ -35,7 +36,7 @@ class ClientEdit:
         self.topLevel.resizable(False, False)
         
         self.contentFrame: tkinter.Frame = tkinter.Frame(self.topLevel, padx=20, pady=10, background="white")
-        self.buttonsFrame: tkinter.Frame = tkinter.Frame(self.topLevel)
+        self.buttonsFrame: tkinter.Frame = tkinter.Frame(self.topLevel)        
 
         # Create all the widgets for the data type and places them and sets the current values
         ## -- Custome per data type between -- ##
@@ -78,14 +79,29 @@ class ClientEdit:
         ## -- Custome per data type between -- ##
         
         # Creats and diplays the save and cancel buttons and also adds the frames to the screen
+        self.newButton: ttk.Button = ttk.Button(self.buttonsFrame, text="New", command=self.__new)
         self.saveButton: ttk.Button = ttk.Button(self.buttonsFrame, text="Save", command=self.__save)
-        self.cancelButton: ttk.Button = ttk.Button(self.buttonsFrame, text="Cancel", command=self.topLevel.destroy)
-        
-        self.saveButton.grid(row=0, column=0, padx=2)
-        self.cancelButton.grid(row=0, column=1, padx=2)
+        self.cancelButton: ttk.Button = ttk.Button(self.buttonsFrame, text="Cancel", command=self.topLevel.destroy)      
+
+        self.newButton.grid(row=0, column=0, padx=2)
+        self.saveButton.grid(row=0, column=1, padx=2)
+        self.cancelButton.grid(row=0, column=2, padx=2)
         
         self.contentFrame.pack()
         self.buttonsFrame.pack(expand=True, anchor="e", padx=15, pady=10)
+       
+    # Used to creae a new version of the data type to be edited
+    def __new(self):
+        with open(self.filePath, "br") as dataFile:
+            data: list[Client] = pickle.load(dataFile)
+            
+        newClient = Client("", "", "", "", "")
+        data.append(newClient)
+        
+        with open(self.filePath, "bw") as dataFile:
+            pickle.dump(data, dataFile)
+            
+        ClientEdit(dict(zip(["id", "first name", "last name", "email", "address", "phone number"], newClient.getAtributes())))
     
     # Used to save the data that was edited
     def __save(self):
@@ -99,14 +115,14 @@ class ClientEdit:
         ## -- Custome per data type between -- ##
 
         # Opens the data type file to retive data types
-        with open("data/clients.pkl", "rb") as dataTypeFile:
+        with open(self.filePath, "rb") as dataTypeFile:
             dataTypes: list = pickle.load(dataTypeFile)
         
         # Replaces the saved version with the updated version
         dataTypes[self.dataTypeIndex:self.dataTypeIndex+1] = [self.currentDataType]
         
         # Saves it back to file and closes the window
-        with open("data/clients.pkl", "wb") as dataTypeFile:
+        with open(self.filePath, "wb") as dataTypeFile:
             pickle.dump(dataTypes, dataTypeFile)
             
         self.topLevel.destroy()

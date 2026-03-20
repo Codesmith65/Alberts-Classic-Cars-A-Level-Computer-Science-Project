8b91fe182aaffec00cef21dfa85a54082861e948
22140733 <22140733@DS-L-107-46711>
2026-03-20 13:41:59 +0000

    Comments

--- screens/login.py
+++ screens/login.py
@@ -21,13 +21,15 @@ class Login(GenericScreen):
 		style.configure("nav.TButton", background=pallet.bg2)
 		style.configure("nav.TEntry", background=pallet.bg2, highlightcolor=pallet.highlight)
 
-
+		# Sets window title
 		self.root.title("Albert's Classic Car - Login")
-
+		
+		# Creates frames
 		self.leftFrame: tk.Frame = tk.Frame(self.root, bg=pallet.bg)
 		self.rightFrame: tk.Frame = tk.Frame(self.root, bg=pallet.bg2)
 		self.loginFrame: tk.Frame = tk.Frame(self.rightFrame, bg=pallet.bg2)
 		
+		# Creates widgets for ui
 		self.companyLogo = tk.PhotoImage(file="assets/logo-large.png")
 		self.companyLogoHomeLabel = tk.Label(self.leftFrame, bg=pallet.bg)
 
@@ -38,6 +40,7 @@ class Login(GenericScreen):
 		self.forgotPasswordLabel: tk.Label = tk.Label(self.loginFrame, bg=pallet.bg2)
 
 
+		# Configures widgets
 		self.loginTitleLabel["text"] = "Login"
 		self.loginTitleLabel["font"] = ("Helvetica", 40, "bold")
 		self.loginButton["text"] = "Login"
@@ -52,6 +55,7 @@ class Login(GenericScreen):
 		self.companyLogoHomeLabel["height"] = 500
 
 
+		# Places widgets on UI
 		self.leftFrame.pack(fill="both", expand=1, side="left")
 		self.rightFrame.pack(fill="y", side="right")
 		self.loginFrame.grid(row=0, column=0, sticky="")
@@ -68,10 +72,13 @@ class Login(GenericScreen):
 		self.loginButton.pack(padx=5, pady=1)
 		self.forgotPasswordLabel.pack(padx=50, pady=1)
 	
+	# Used to log the user in
 	def login(self) -> None:
+		# Gets the entered username and password
 		username: str = self.usernameEntry.get()
 		password: str = self.passwordEntry.get()
 		
+		# Opnes the users and staff data files to get data
 		users: list[User] = []
 		staffs: list[Staff] = []
 		with open("data/users.pkl", "rb") as userFile:
@@ -79,7 +86,8 @@ class Login(GenericScreen):
 		
 		with open("data/staff.pkl", "rb") as staffFile:
 			staffs = pickle.load(staffFile)
-			
+		
+		# Loops through every user and checks the credentials of the user if they dont match continue otherwise check the staff to find the associated staff member to the user
 		for user in users:
 			if not user.checkCredentials(username, password):
 				continue
@@ -88,8 +96,10 @@ class Login(GenericScreen):
 				if staff.userID == user.userID:
 					self.application.setLoggedInStaff(staff.staffID)
 			
+			# Sets the loged in user and takes to the home screen
 			self.application.setLoggedInUser(user.userID)
 			self.application.switchForm(screens.Home)
 			return
 		
+		# Shows a error if user name or password incorect
 		messagebox.showwarning("Incorect username or password", "The username or password is incorrect, please ensure that the details are correct")

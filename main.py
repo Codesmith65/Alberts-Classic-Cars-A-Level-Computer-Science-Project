8089141fa12a3e29d7859daa6307f401b3c3ae47
22140733 <22140733@DS-L-107-46711>
2026-03-20 15:53:04 +0000

    testing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import uuid
 
 
 application = Application()
-application.switchForm(screens.Invoice, {"BookingID": uuid.UUID("9cca727b-5030-487b-b6d6-b346064c9bdb")})
+application.switchForm(screens.Task, {"BookingID": uuid.UUID("9cca727b-5030-487b-b6d6-b346064c9bdb")})
 
 while application.running:
     application.currentScreen.mainLoop()

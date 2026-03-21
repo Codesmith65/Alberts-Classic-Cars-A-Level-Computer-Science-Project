24934de27ac2ee74eaf9983056a1724a681a613a
Codesmith65 <187884700+Codesmith65@users.noreply.github.com>
2026-03-21 19:24:11 +0000

    Ready for testing and final

--- application.py
+++ application.py
@@ -8,30 +8,38 @@ from uuid import UUID
 
 class Application:
     def __init__(self) -> None:
-        self.logedInUser: UUID|None = UUID("d8eec26b-91c3-4144-b2f7-97ec1f20d064")
-        self.loggedInStaff: UUID|None = UUID("e22e4c85-589f-4370-b052-599cb3dabbc9")
+        self.logedInUser: UUID|None
+        self.loggedInStaff: UUID|None
         self.currentScreen: GenericScreen|None = None
         self.crossScreenDataStore: dict = {}
         
         self.running: bool = True
     
+    # Sets the user which is loged in
     def setLoggedInUser(self, userID: UUID|None) -> None:
         self.logedInUser = userID
     
+    # Sets the staf which is loged in
     def setLoggedInStaff(self, staffID: UUID|None) ->None:
         self.loggedInStaff = staffID
 
+    # Swu=itches form
     def switchForm(self, newForm: GenericScreen, crossScreenData: dict = {}) -> None:
+        # Crears the data store and sets it to new data
         self.crossScreenDataStore.clear()
         self.crossScreenDataStore = crossScreenData
 
+        # Destroys the old screen
         if self.currentScreen != None:
             self.currentScreen.root.destroy()
         
+        # Opems the new screen
         self.currentScreen: GenericScreen = newForm(self)
         
+        # Attaches to the close event
         self.currentScreen.root.protocol("WM_DELETE_WINDOW", self.closeWindow)
     
+    # Attached to the close event to know when to stop the infinte loop and destroys the window
     def closeWindow(self):
         self.running = False
         self.currentScreen.root.destroy()

05226cf803fb8d91f33f9c0acb3cb77cccd8ff7a
Codesmith65 <187884700+Codesmith65@users.noreply.github.com>
2026-03-21 19:01:49 +0000

    Prepared for testing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import uuid
 
 
 application = Application()
-application.switchForm(screens.Task, {"BookingID": uuid.UUID("9cca727b-5030-487b-b6d6-b346064c9bdb")})
+application.switchForm(screens.Login)
 
 while application.running:
     application.currentScreen.mainLoop()

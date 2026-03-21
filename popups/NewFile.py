28d2620111df744d9e25280b13802881d5775db2
Codesmith65 <187884700+Codesmith65@users.noreply.github.com>
2026-03-21 18:59:37 +0000

    testing

--- popups/NewFile.py
+++ popups/NewFile.py
@@ -1,7 +1,6 @@
 import pickle
 import tkinter
 from tkinter import ttk, messagebox
-from turtle import back
 
 from dataTypes.client import Client
 
@@ -10,7 +9,8 @@ class ClientEdit:
     # Initilaistion function to creat the screen
     def __init__(self, data) -> None:
         # Open the data file find the data type
-        with open("data/clients.pkl", "rb") as dataFile:
+        self.filePath = "data/clients.pkl"
+        with open(self.filePath, "rb") as dataFile:
             dataTypes = pickle.load(dataFile)
         
         # Find the current user to edit in the file and its index
@@ -21,7 +21,7 @@ class ClientEdit:
                 self.currentDataType = dataType
                 break
             
-            self.userIndex += 1
+            self.dataTypeIndex += 1
         
         # Shows an error if the data type cannot be found
         if self.currentDataType == None:
@@ -35,7 +35,7 @@ class ClientEdit:
         self.topLevel.resizable(False, False)
         
         self.contentFrame: tkinter.Frame = tkinter.Frame(self.topLevel, padx=20, pady=10, background="white")
-        self.buttonsFrame: tkinter.Frame = tkinter.Frame(self.topLevel)
+        self.buttonsFrame: tkinter.Frame = tkinter.Frame(self.topLevel)        
 
         # Create all the widgets for the data type and places them and sets the current values
         ## -- Custome per data type between -- ##
@@ -78,13 +78,17 @@ class ClientEdit:
         ## -- Custome per data type between -- ##
         
         # Creats and diplays the save and cancel buttons and also adds the frames to the screen
+        self.newButton: ttk.Button = ttk.Button(self.buttonsFrame, text="New", command=self.__new)
+        self.deleteButton: ttk.Button = ttk.Button(self.buttonsFrame, text="Delete", command=self.__delete)
         self.saveButton: ttk.Button = ttk.Button(self.buttonsFrame, text="Save", command=self.__save)
-        self.cancelButton: ttk.Button = ttk.Button(self.buttonsFrame, text="Cancel", command=self.topLevel.destroy)
+        self.cancelButton: ttk.Button = ttk.Button(self.buttonsFrame, text="Cancel", command=self.topLevel.destroy)      
+
+        self.newButton.grid(row=0, column=0, padx=2)
+        self.deleteButton.grid(row=0, column=1, padx=2)
+        self.saveButton.grid(row=0, column=2, padx=2)
+        self.cancelButton.grid(row=0, column=3, padx=2)
         
-        self.saveButton.grid(row=0, column=0, padx=2)
-        self.cancelButton.grid(row=0, column=1, padx=2)
-        
-        self.contentFrame.pack()
+        self.contentFrame.pack(fill="x")
         self.buttonsFrame.pack(expand=True, anchor="e", padx=15, pady=10)
     
     # Used to save the data that was edited
@@ -99,14 +103,39 @@ class ClientEdit:
         ## -- Custome per data type between -- ##
 
         # Opens the data type file to retive data types
-        with open("data/clients.pkl", "rb") as dataTypeFile:
+        with open(self.filePath, "rb") as dataTypeFile:
             dataTypes: list = pickle.load(dataTypeFile)
         
         # Replaces the saved version with the updated version
         dataTypes[self.dataTypeIndex:self.dataTypeIndex+1] = [self.currentDataType]
         
         # Saves it back to file and closes the window
-        with open("data/clients.pkl", "wb") as dataTypeFile:
+        with open(self.filePath, "wb") as dataTypeFile:
             pickle.dump(dataTypes, dataTypeFile)
             
         self.topLevel.destroy()
+    
+    # Used to creae a new version of the data type to be edited
+    def __new(self):
+        with open(self.filePath, "br") as dataFile:
+            data: list[Client] = pickle.load(dataFile)
+            
+        newClient = Client("", "", "", "", "")
+        data.append(newClient)
+        
+        with open(self.filePath, "bw") as dataFile:
+            pickle.dump(data, dataFile)
+            
+        ClientEdit(dict(zip(["id", "first name", "last name", "email", "address", "phone number"], newClient.getAtributes())))
+    
+    def __delete(self):
+        # Opens the data type file to retive data types
+        with open(self.filePath, "rb") as dataTypeFile:
+            dataTypes: list = pickle.load(dataTypeFile)
+
+        dataTypes.pop(self.dataTypeIndex)
+        self.topLevel.destroy()
+
+        # Saves it back to file and closes the window
+        with open(self.filePath, "wb") as dataTypeFile:
+            pickle.dump(dataTypes, dataTypeFile)
